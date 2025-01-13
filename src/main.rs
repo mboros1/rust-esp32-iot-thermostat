8d6b80a9242bccfd6d7a62ec27512d3894c831ef
Martin Boros <mboros04@gmail.com>
2025-01-12 22:39:32 -0500

    have a working thermostat now using the esp32 onewire_bus code

--- src/main.rs
+++ src/main.rs
@@ -49,5 +49,74 @@ fn main() {
         device,
         device.family_code()
     );
+
+    loop {
+        ds18b20_trigger_temp_conversion(&device, &onewire_bus).unwrap();
+        let temp = ds18b20_get_temperature(&device, &onewire_bus).unwrap();
+        println!("Temperature: {} C, {} F", temp, c_to_f(temp));
+        FreeRtos::delay_ms(3000);
+    }
 }
 
+pub fn c_to_f(celsius: f32) -> f32 {
+    celsius * 9.0 / 5.0 + 32.0
+}
+
+#[cfg(all(
+    esp_idf_soc_rmt_supported,
+    not(esp_idf_version_major = "4"),
+    esp_idf_comp_espressif__onewire_bus_enabled,
+))]
+fn ds18b20_send_command<'a>(addr: &OWAddress, bus: &OWDriver, cmd: u8) -> Result<(), EspError> {
+    let mut buf = [0; 10];
+    buf[0] = OWCommand::MatchRom as _;
+    let addr = addr.address().to_le_bytes();
+    buf[1..9].copy_from_slice(&addr);
+    buf[9] = cmd;
+
+    bus.write(&buf)
+}
+
+#[allow(dead_code)]
+#[repr(u8)]
+enum Ds18b20Command {
+    ConvertTemp = 0x44,
+    WriteScratch = 0x4E,
+    ReadScratch = 0xBE,
+}
+#[cfg(all(
+    esp_idf_soc_rmt_supported,
+    not(esp_idf_version_major = "4"),
+    esp_idf_comp_espressif__onewire_bus_enabled,
+))]
+fn ds18b20_trigger_temp_conversion<'a>(addr: &OWAddress, bus: &OWDriver) -> Result<(), EspError> {
+    // reset bus and check if the ds18b20 is present
+    bus.reset()?;
+
+    ds18b20_send_command(addr, bus, Ds18b20Command::ConvertTemp as u8)?;
+
+    // delay proper time for temp conversion,
+    // assume max resolution (12-bits)
+    std::thread::sleep(Duration::from_millis(800));
+
+    Ok(())
+}
+#[cfg(all(
+    esp_idf_soc_rmt_supported,
+    not(esp_idf_version_major = "4"),
+    esp_idf_comp_espressif__onewire_bus_enabled,
+))]
+fn ds18b20_get_temperature<'a>(addr: &OWAddress, bus: &OWDriver) -> Result<f32, EspError> {
+    bus.reset()?;
+
+    ds18b20_send_command(addr, bus, Ds18b20Command::ReadScratch as u8)?;
+
+    let mut buf = [0u8; 9];
+    bus.read(&mut buf)?;
+    let lsb = buf[0];
+    let msb = buf[1];
+
+    let temp_raw: u16 = (u16::from(msb) << 8) | u16::from(lsb);
+
+    Ok(f32::from(temp_raw) / 16.0)
+}
